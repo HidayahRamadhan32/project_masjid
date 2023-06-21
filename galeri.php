3b63f9793aaa156f06cb93a8c00d6b386113edc8
HidayahRamadhan32 <137246108+HidayahRamadhan32@users.noreply.github.com>
2023-06-21 09:14:58 +0700

    Add files via upload

---
+++ galeri.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="galeri">
+        <div class="header-text-1">
+        <div class="text1"><br><h1><span>Gallery </span> Masjid Besar SMK <br> Wikrama Bogor.</h1><br>
+        <p>Berikut adalah gallery masjid besar SMK Wikrama Bogor.</p><br><br>
+        </div>
+        </div> 
+        <div class="galeri"></div> 
+            <div class="gallery">
+             
+              <img src="img/mekkah.jpeg" alt="Above Photo 1">
+              <img src="img/mekkah.jpeg" alt="Above Photo 2">
+              <img src="img/mekkah.jpeg" alt="Above Photo 3">
+            </div>
+
+            <div class="gallery">
+             
+              <img src="img/mekkah.jpeg" alt="Above Photo 1">
+              <img src="img/mekkah.jpeg" alt="Above Photo 2">
+              <img src="img/mekkah.jpeg" alt="Above Photo 3">
+            </div>
+            
+
+            <div class="gallery">
+              
+              <img src="img/mekkah.jpeg" alt="Below Photo 1">
+              <img src="img/mekkah.jpeg" alt="Below Photo 2">
+              <a href="index.php"><img src="img/kembali.jpeg" alt="Below Photo 3"></a>
+            </div>
+</body>
+</html>
